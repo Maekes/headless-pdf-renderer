973f0ea07c83a4388a1f8557e977617bccfaa1ff
Markus Marcinek <markus.marcinek@outlook.de>
2021-12-20 16:16:09 +0100

    let browser open

--- src/index.js
+++ src/index.js
@@ -1,51 +1,58 @@
-import 'core-js'
-import 'regenerator-runtime'
-import puppeteer from 'puppeteer'
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import 'core-js';
+import 'regenerator-runtime';
+import puppeteer from 'puppeteer';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.post('/render', async (req, res) => {
-  res.setHeader('Content-type', 'application/pdf')
-  res.send(await renderPDF(req.body.html))
-})
+    res.setHeader('Content-type', 'application/pdf');
+    res.send(await renderPDF(req.body.html));
+});
 
 app.listen(8082, () => {
-  console.log('Listening on port 8082')
-})
+    console.log('Listening on port 8082');
+});
 
 process.on('SIGINT', function () {
-  process.exit()
-})
+    process.exit();
+});
 
-async function renderPDF(html) {
-  const browser = await puppeteer.launch({
+let browser = await puppeteer.launch({
     headless: true,
     executablePath: 'google-chrome-stable',
-    args: [
-      '--disable-dev-shm-usage',
-    ]
-  })
-  const page = await browser.newPage()
-  await page.setContent(html, {waitUntil: 'networkidle0'})
-  const pdf = await page.pdf({
-    printBackground: true,
-    format: 'A4',
-    preferCSSPageSize: true,
-    margin: {
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-    },
-  })
+    args: ['--disable-dev-shm-usage'],
+});
 
-  await browser.close()
+async function renderPDF(html) {
+    if (!browser.isConnected()) {
+        browser = await puppeteer.launch({
+            headless: true,
+            executablePath: 'google-chrome-stable',
+            args: ['--disable-dev-shm-usage'],
+        });
+    }
 
-  return pdf
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+    const pdf = await page.pdf({
+        printBackground: true,
+        format: 'A4',
+        preferCSSPageSize: true,
+        margin: {
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+        },
+    });
+
+    await page.close();
+
+    return pdf;
 }
